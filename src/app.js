eee34f64de53de159467f5269c9f8c493947fe11
edwin <edwin.satoritech@gmail.com>
2023-04-15 17:50:13 -0600

    table name

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import {PORT} from './config.js'
 const app = express()
 
 app.get('/', async (req, res) => {
-  const [rows] = await pool.query('SELECT * FROM users')
+  const [rows] = await pool.query('SELECT * FROM user')
   res.json(rows)
 })
 
