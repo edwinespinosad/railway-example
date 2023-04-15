c04acebd6e975ddc11fbe47e17a02419dd00ed31
edwin <edwin.satoritech@gmail.com>
2023-04-15 17:45:03 -0600

    change db

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.get('/ping', async (req, res) => {
 })
 
 app.get('/create', async (req, res) => {
-  const result = await pool.query('INSERT INTO users(name) VALUES ("John")')
+  const result = await pool.query('INSERT INTO user(name) VALUES ("John")')
   res.json(result)
 })
 
